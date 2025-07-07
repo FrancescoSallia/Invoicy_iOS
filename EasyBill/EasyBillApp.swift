ba56267688597bf4cb5bc7efcce9a91732404db8
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-07-07 17:25:46 +0200

    Initial Commit

---
+++ EasyBill/EasyBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBillApp.swift
+//  EasyBill
+//
+//  Created by Francesco Sallia on 07.07.25.
+//
+
+import SwiftUI
+
+@main
+struct EasyBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
