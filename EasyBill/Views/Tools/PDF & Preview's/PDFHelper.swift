dda11185832497be3d77671615778d36cd39972a
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-07-30 17:36:12 +0200

    pdf druck function created

--- EasyBill/Views/Tools/PDF & Preview's/PDFHelper.swift
+++ EasyBill/Views/Tools/PDF & Preview's/PDFHelper.swift
@@ -10,7 +10,23 @@ import PDFKit
 
 // PDF-Helfer zur Erzeugung aus HTML
 struct PDFHelper {
+    
+    //Generiert eine PDF Vorschau
     static func generatePDF(from invoice: Invoice, with viewModel: BillViewModel) -> Data {
         return InvoicePrinter(viewModel: viewModel).generateInvoicePDF(invoice)
     }
+    
+    // Diese Funktion dient dazu das man die ganze PDF Datei denn auch drucken kann.
+    static func printPDF(data: Data) {
+          let printController = UIPrintInteractionController.shared
+          let printInfo = UIPrintInfo(dictionary: nil)
+          printInfo.jobName = "Rechnung drucken"
+          printInfo.outputType = .general
+          printController.printInfo = printInfo
+          printController.printingItem = data
+
+          DispatchQueue.main.async {
+              printController.present(animated: true, completionHandler: nil)
+          }
+      }
 }
--- EasyBill/Views/Tools/PDF & Preview's/PreviewScreen.swift
+++ EasyBill/Views/Tools/PDF & Preview's/PreviewScreen.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct PreviewScreen: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) var context
+    
     let invoice: Invoice
     let viewModel: BillViewModel
 
     var body: some View {
+        Spacer()
+
         let pdfData = PDFHelper.generatePDF(from: invoice, with: viewModel)
         PDFPreviewView(pdfData: pdfData)
             .edgesIgnoringSafeArea(.all)
+        
+        ScrollView(.horizontal) {
+            HStack {
+                Button {
+                    PDFHelper.printPDF(data: pdfData)
+                } label: {
+                    Image(systemName: "printer.fill")
+                }
+                
+                Button {
+                    context.delete(invoice)
+                    try? context.save()
+                    dismiss()
+                } label: {
+                    Image(systemName: "trash.fill")
+                }
+            }
+        }
+        Spacer()
     }
 }
